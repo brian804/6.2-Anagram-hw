515b24991ea160d82ad7794c6dc30b0d17a70671
Brian Rivera <82990606+brian804@users.noreply.github.com>
2023-01-11 11:34:20 -0500

    6.2 anagram hw

---
+++ 6.2 anagram hw.js
@@ -0,0 +1,11 @@
+function checkStringsAnagram(a, b) {
+    let str1 = a.split('').sort().join('');
+    let str2 = b.split('').sort().join('');
+    if(str1 === str2){
+       console.log("True");
+    } else { 
+       console.log("False");
+    }
+ }
+ checkStringsAnagram("test","tste")
+
